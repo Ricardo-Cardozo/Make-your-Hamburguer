9a35e01ca325bd1e0675f0ac3f8df0fad4a5ce70
Ricardo Cardozo <ricardo_alexandrecardozo@hotmail.com>
2023-01-24 18:46:34 -0300

    teste 4

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -48,7 +48,7 @@
     },
     methods: {
       async getPedidos() {
-        const req = await fetch('http://localhost:3000/burgers')
+        const req = await fetch('https://make-your-hamburguer.vercel.app/burgers')
 
         const data = await req.json()
 
@@ -61,7 +61,7 @@
       },
       async getStatus() {
 
-        const req = await fetch('http://localhost:3000/status')
+        const req = await fetch('https://make-your-hamburguer.vercel.app/status')
 
         const data = await req.json()
 
@@ -70,7 +70,7 @@
       },
       async deleteBurger(id) {
 
-        const req = await fetch(`http://localhost:3000/burgers/${id}`, {
+        const req = await fetch(`https://make-your-hamburguer.vercel.app/burgers/${id}`, {
           method: "DELETE"
         });
 
@@ -85,7 +85,7 @@
 
         const dataJson = JSON.stringify({status: option});
 
-        const req = await fetch(`http://localhost:3000/burgers/${id}`, {
+        const req = await fetch(`https://make-your-hamburguer.vercel.app/burgers/${id}`, {
           method: "PATCH",
           headers: { "Content-Type" : "application/json" },
           body: dataJson
